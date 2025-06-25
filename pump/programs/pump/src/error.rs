6b8be06e722b0511b52ca09f91b54713e0484d0c
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-25 10:32:37 +0000

    feat: created account structs in the src/state folder

--- pump/programs/pump/src/error.rs
+++ pump/programs/pump/src/error.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+    #[msg("Overflow detected")]
+    OverflowDetected,
+    #[msg("Underflow detected")]
+    UnderflowDetected,
+    #[msg("the token amount can't be zero")]
+    InvalidTokenAmount,
+    #[msg("the sol amount can't be zero")]
+    InvalidSolAmount,
 }
