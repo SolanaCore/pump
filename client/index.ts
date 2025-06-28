82883f1e111f5675f949d5a2aee440df261ce36d
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-28 19:10:14 +0000

    added scripts for updating package version and publishing the package

--- client/index.ts
+++ client/index.ts
@@ -1,15 +1,15 @@
 import { AnchorIdl, rootNodeFromAnchorWithoutDefaultVisitor } from "@codama/nodes-from-anchor";
 import { renderJavaScriptUmiVisitor, renderJavaScriptVisitor, renderRustVisitor } from "@codama/renderers";
 import { visit } from "@codama/visitors-core";
-import anchorIdl from "../target/idl/pump.json"; 
+import anchorIdl from "../idl/pump.json"; 
 
 async function generateClients() {
     const node = rootNodeFromAnchorWithoutDefaultVisitor(anchorIdl as AnchorIdl);
 
     const clients = [
-        { type: "JS", dir: "clients/generated/js/src", renderVisitor: renderJavaScriptVisitor },
-        { type: "Umi", dir: "clients/generated/umi/src", renderVisitor: renderJavaScriptUmiVisitor },
-        { type: "Rust", dir: "clients/generated/rust/src", renderVisitor: renderRustVisitor }
+        { type: "JS", dir: "./js/src", renderVisitor: renderJavaScriptVisitor },
+        { type: "Umi", dir: "./umi/src", renderVisitor: renderJavaScriptUmiVisitor },
+        { type: "Rust", dir: "./rust/src", renderVisitor: renderRustVisitor }
     ];
 
     for (const client of clients) {
